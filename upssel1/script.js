cfddaf8da148ebcd3c1407da4e51aef5d53bff99
FranciscoRoges09 <rogesfrancisco695@gmail.com>
2025-07-31 06:24:58 -0300

    Corrigindo redirecionamento no botão Adquirir Oferta

--- upssel1/script.js
+++ upssel1/script.js
@@ -1,5 +1,3 @@
 function adquirirOferta() {
-  alert('Oferta adicionada ao seu pedido com sucesso!');
-  // Aqui você pode adicionar redirecionamento para próxima etapa
-  // window.location.href = 'https://checkout.desafiodeagosto.online/VCCL1O8SC4I8';
+  window.location.href = 'https://checkout.desafiodeagosto.online/VCCL1O8SC4I8';
 }
